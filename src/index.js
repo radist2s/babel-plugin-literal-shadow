b38d382eb1f6cc7efc365772006743f8d6bb1729
Alex Batalov <radist2s@gmail.com>
2020-03-26 21:55:18 +0500

    literalShadow dummy function error reporting added in case of wrong babel configuration

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@
  * @return {string}
  */
 export default function literalShadow(strings, ...args) {
-    return args[0].displayName || args[0].name || args[0] // nothing will
+    throw new Error('This function should not exist in compiled code. Make sure babel is using `@radist2s/babel-plugin-literal-shadow/babel`')
 }
